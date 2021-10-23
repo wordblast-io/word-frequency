f2ffab69e141f98ad5372fd79dcf9c600d23ce14
Emilio Barradas <decalerss@gmail.com>
2021-10-23 18:25:36 -0400

    Implement frequency threshold.

--- src/words.ts
+++ src/words.ts
@@ -1,11 +1,14 @@
 import { readFile, writeFile } from "fs/promises";
 
 const INPUT_WORDS_PATH = "./input/words.txt";
-const OUTPUT_FREQ_PATH = "./output/words.json";
+const OUTPUT_FREQ_DEFUALT_PATH = "./output/words.json";
+const OUTPUT_FREQ_SORTED_PATH = "./output/words_sorted.json";
 const WORD_SEPARATOR = "\r\n";
 const COMBO_LENGTHS = [2, 3, 4];
+const FREQ_THRESHOLD = 100;
 
-const output = {};
+const defaultOutput = {};
+const sortedOutput = {};
 
 const data = await readFile(INPUT_WORDS_PATH, "utf-8");
 const words = data.split(WORD_SEPARATOR);
@@ -25,7 +28,16 @@ for (const length of COMBO_LENGTHS) {
 			frequencies[combo] = (frequencies[combo] || 0) + 1;
 	}
 
-	output[length] = Object.entries(frequencies).sort((a, b) => b[1] - a[1]);
+	for (const [combo, freq] of Object.entries(frequencies)) {
+		if (freq >= FREQ_THRESHOLD) continue;
+		delete frequencies[combo];
+	}
+
+	defaultOutput[length] = frequencies;
+	sortedOutput[length] = Object.entries(frequencies).sort(
+		(a, b) => b[1] - a[1]
+	);
 }
 
-writeFile(OUTPUT_FREQ_PATH, JSON.stringify(output));
+writeFile(OUTPUT_FREQ_DEFUALT_PATH, JSON.stringify(defaultOutput));
+writeFile(OUTPUT_FREQ_SORTED_PATH, JSON.stringify(sortedOutput));
